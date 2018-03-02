d941f5193e6a732c8f29600a2c5f52f9c2c6a297
FatmeMustafa <fatme.mustafa888@gmail.com>
2018-03-02 22:45:46 +0100

    fixed bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,59 @@
+$(function() {
+
 /***********************Showing Registration Form***********************/
 
-let registrationPart = document.getElementById("register");
-    registrationPart.addEventListener("click", function() {
-    document.getElementsByClassName("landingpagesection")[0].style.display = "none";
-    document.getElementById("registrationForm").style.display = "block";    
+$("#register").on("click", function () {
+    $("#registrationForm").show();
+    $(".landingpagesection").hide();
 });
 
-/********************Validating Registration Form**********************/
+/*********************Validating Registration Form**********************/
 
-function formValidation () {
-    let userUsername = document.forms["registrationForm"]["userName"].value;
-    if (userUsername == "" || isNaN(userUsername) === false) {  //checks if input is missing or is a number
-        document.getElementById("error1").innerHTML = "* ";
-        document.getElementById("userName").style.borderColor = "red";
-    } 
-    else {  
-        document.getElementById("error1").innerHTML = "";
-        document.getElementById("userName").style.borderColor = "#ccc";
+$("#registerBTN").on("click", function () {
+
+let usersUsername = $("#userName").val();
+    if (usersUsername === "" || isNaN(usersUsername) === false) {       //checks if input field is missing or is a number.
+        $("#error1").text(" *");
+        $("#userName").css({"borderColor": "red"});
+    } else {
+        $("#error1").text("");
+        $("#userName").css({"borderColor": "#ccc"});
+    }     
+let usersFullName = $("#fullName").val();          
+    if (usersFullName === "" || isNaN(usersFullName) === false || usersFullName.split(" ").length === 1) {      //checks if input field is missing, is a number or contains two strings.  
+        $("#error2").text(" *");
+        $("#fullName").css({"borderColor": "red"});
+    } else {
+        $("#error2").text("");
+        $("#fullName").css({"borderColor": "#ccc"});
     }
-    let userFullName = document.forms["registrationForm"]["fullName"].value;
-    if (userFullName == "" || isNaN(userFullName) === false) {  
-        document.getElementById("error2").innerHTML = "* ";
-        document.getElementById("fullName").style.borderColor = "red";
-    } 
-    else {  
-        document.getElementById("error2").innerHTML = "";
-        document.getElementById("fullName").style.borderColor = "#ccc";
+let usersEmail = $("#email").val(); 
+    if (usersEmail === "" || usersEmail != usersEmail.match(/^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/)) {     //checks if the input field is missing or contains an invalid email adress.
+        $("#error3").text(" *");
+        $("#email").css({"borderColor": "red"});
+    } else {
+        $("#error3").text("");
+        $("#email").css({"borderColor": "#ccc"});
     }
-    let userEmail = document.forms["registrationForm"]["email"].value;
-    if (userEmail == "") {  
-        document.getElementById("error3").innerHTML = "* ";
-        document.getElementById("email").style.borderColor = "red";
-    } 
-    else {  
-        document.getElementById("error3").innerHTML = "";
-        document.getElementById("email").style.borderColor = "#ccc";
+let usersPassword = $("#password").val();
+    if (usersPassword === "" || usersPassword.length < 6) {     //checks if the input field is missing or has a password below 6 charecters
+        $("#error4").text(" *");
+        $("#password").css({"borderColor": "red"});
+    } else {
+        $("#error4").text("");
+        $("#password").css({"borderColor": "#ccc"});
     }
-    let userPassword = document.forms["registrationForm"]["password"].value;
-    if (userPassword == "") {  
-        document.getElementById("error4").innerHTML = "* ";
-        document.getElementById("password").style.borderColor = "red";
-    } 
-    else {  
-        document.getElementById("error4").innerHTML = "";
-        document.getElementById("password").style.borderColor = "#ccc";
+let usersRegistrationMessage = $("#registrationMessage")
+    if (usersUsername === "" || isNaN(usersUsername) === false ||
+    usersFullName === "" || isNaN(usersFullName) === false || usersFullName.split(" ").length === 1 ||
+    usersEmail === "" || usersEmail != usersEmail.match(/^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/) ||
+    usersPassword === "" || usersPassword.length < 6) {
+        $("#registrationMessage").text("Please fill in the required fields.");
+        $("#registrationMessage").css({"color": "red"});
+    } else {
+        $("#registrationMessage").text("Thank you for joining us!");
+        $("#registrationMessage").css({"color": "green"});
     }
-    if (userUsername == "" || isNaN(userUsername) === false || userFullName == "" || isNaN(userFullName) === false || userEmail == "" || userPassword == "") {
-        document.getElementById("registrationMessage").innerHTML = "Please fill in the required fields.";
-    }
-    else {
-        document.getElementById("registrationMessage").innerHTML = "Thanks for registrating!";
-        document.getElementById("registrationMessage").style.color = "green";
-    }
-}
-document.getElementById('submitBTN').addEventListener("click", formValidation);
+});
+
+});

78ca059c4ffa13f25c29eb26b3c5308290457be8
FatmeMustafa <fatme.mustafa888@gmail.com>
2018-03-05 15:20:46 +0100

    login validation

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,28 @@
 $(function() {
 
-/***********************Showing Registration Form***********************/
+/***********************Showing And Hiding Elements**********************/
 
-$("#register").on("click", function () {       //REGISTER button.  
+$("#register").on("click", function () {       //showing registration page when clicking the registration button.  
     $("#registrationForm").show();
     $(".landingpagesection").hide();
 });
 
-$(".back").on("click", function () {        //BACK button.
+$(".back").on("click", function () {        //showing landing page when clicking the back button.
     $("#registrationForm").hide();
+    $("#loginForm").hide();
     $(".landingpagesection").show();
+});
+
+$("#login").on("click", function () {       //showing login page when clicking the login button.  
+    $("#loginForm").show();
+    $(".landingpagesection").hide();
 });
 
 /*********************Validating Registration Form**********************/
 
 $("#registerBTN").on("click", function () {
 
-let usersUsername = $("#userName").val();
+    let usersUsername = $("#userName").val();
     if (usersUsername === "" || isNaN(usersUsername) === false) {       //checks if input field is missing or is a number.
         $("#error1").text(" *");
         $("#userName").css({"borderColor": "red"});
@@ -52,7 +58,7 @@ let usersRegistrationMessage = $("#registrationMessage")
     if (usersUsername === "" || isNaN(usersUsername) === false ||
     usersFullName === "" || isNaN(usersFullName) === false || usersFullName.split(" ").length === 1 ||
     usersEmail === "" || usersEmail != usersEmail.match(/^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/) ||
-    usersPassword === "" || usersPassword.length < 6) {
+    usersPassword === "" || usersPassword.length < 6) {     //checks if the input fields fulfils the specified requirements.
         $("#registrationMessage").text("Please fill in the required fields.");
         $("#registrationMessage").css({"color": "red"});
     } else {
@@ -60,5 +66,36 @@ let usersRegistrationMessage = $("#registrationMessage")
         $("#registrationMessage").css({"color": "green"});
     }
 });
+
+/*************************Validating Login Form*************************/
+
+$("#loginBTN").on("click", function () {
+let usersUsernameLogin = $("#userNameLogin").val();
+    if (usersUsernameLogin === "" || isNaN(usersUsernameLogin) === false) {       //checks if input field is missing or is a number.
+        $("#error1Login").text(" *");
+        $("#userNameLogin").css({"borderColor": "red"});
+    } else {
+        $("#error1Login").text("");
+        $("#userNameLogin").css({"borderColor": "#ccc"});
+    }
+let usersPasswordLogin = $("#passwordLogin").val();
+    if (usersPasswordLogin === "" || usersPasswordLogin.length < 6) {     //checks if the input field is missing or has a password below 6 charecters
+        $("#error2Login").text(" *");
+        $("#passwordLogin").css({"borderColor": "red"});
+    } else {
+        $("#error2Login").text("");
+        $("#passwordLogin").css({"borderColor": "#ccc"});
+    }
+let usersLoginMessage = $("#loginMessage")
+    if (usersUsernameLogtin === "" || isNaN(usersUsernameLogin) === false ||
+    usersPasswordLogin === "" || usersPasswordLogin.length < 6) {     //checks if the input fields fulfils the specified requirements.
+        $("#loginMessage").text("Please fill in the required fields.");
+        $("#loginMessage").css({"color": "red"});
+    } else {
+        $("#loginMessage").text("Welcome back!");
+        $("#loginMessage").css({"color": "green"});
+    }
+});
+
 
 });

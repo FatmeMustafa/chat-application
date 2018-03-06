a8697ff4feaa7cb4f463c852234e64d06ecaec53
FatmeMustafa <fatme.mustafa888@gmail.com>
2018-03-06 17:06:47 +0100

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,12 @@ $(function() {
     let config = {
         apiKey: "AIzaSyAn9NuvsF2kiWAgcB4yrXqwJKHK-ZCFEL8",
         authDomain: "chat.firebaseapp.com",
-        databaseURL: "https://Chat.firebaseio.com",
+        //databaseURL: "https://Chat.firebaseio.com", firestore
       };
       firebase.initializeApp(config);
 
 //     const auth = firebase.auth();
-      const db = firebase.database();
+      const db = firebase.firestore();
 
      
 
@@ -18,17 +18,23 @@ $(function() {
 $("#register").on("click", function () {       //showing registration page when clicking the registration button.  
     $("#registrationForm").show();
     $(".landingpagesection").hide();
+    $(".lobbyRoom").hide(); //change this!
+    $(".chatRoom").hide();
 });
 
 $(".back").on("click", function () {        //showing landing page when clicking the back button.
     $("#registrationForm").hide();
     $("#loginForm").hide();
     $(".landingpagesection").show();
+    $(".lobbyRoom").hide();
+    $(".chatRoom").hide();
 });
 
 $("#login").on("click", function () {       //showing login page when clicking the login button.  
     $("#loginForm").show();
     $(".landingpagesection").hide();
+    $(".lobbyRoom").hide();
+    $(".chatRoom").hide();
 });
 
 /*********************Validating Registration Form**********************/
@@ -56,7 +62,7 @@ $("#registerBTN").on("click", function (e) {
         $("#error2").text("");
         $("#fullName").css({"borderColor": "#ccc"});
     }
-    usersEmail = $("#email").val(); 
+    usersEmail = $("#email").val(); //IF EMAIL ALREADY EXISTS! CHANGE!
     if (usersEmail === "" || usersEmail != usersEmail.match(/^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/)) {     //checks if the input field is missing or contains an invalid email adress.
         $("#error3").text(" *");
         $("#email").css({"borderColor": "red"});
@@ -85,24 +91,79 @@ let usersRegistrationMessage = $("#registrationMessage")
     }
     const auth = firebase.auth();
     const promise = auth.createUserWithEmailAndPassword(usersEmail, usersPassword);
-
-
 });
 
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
+
     if (firebaseUser) {
-        console.log(firebaseUser);
+      console.log(firebaseUser.email);
+        console.log("YOU ARE LOGGED IN!");
+        $("#loggedInUser").text(`logged in as "${firebaseUser.email}"`)
+
+//        let LoggedInUser = $("#loggedInUser").val()
+       $(".landingpagesection").hide();
+       $("#registrationForm").hide();
+       $("#loginForm").hide();
+       $(".lobbyRoom").show();
+       $(".chatRoom").hide();
+
+
     } else {
         console.log("not logged in!");
+        $(".lobbyRoom").hide();
+        $(".chatRoom").hide();
     }
 });
 
 
+/************chatroom event*********************/
+$("#chatroom1").on("click", function() {
+    $(".landingpagesection").hide();
+    $("#registrationForm").hide();
+    $("#loginForm").hide();
+    $(".lobbyRoom").hide();
+    $(".chatRoom").show();
+});
+
+$(".send").on("click", function() {
+    let currentUser = firebaseUser.email;
+    let currentText = $(".message").val();
+    //config.databaseURL.push({user: currentUser, text: currentText});
+    db.push({user: currentUser, text: currentText});
+    currentText.val("");
+});
+/***********************************************/
+
+$(".logout").on("click", e => {
+    firebase.auth().signOut();
+    $(".landingpagesection").show();
+
+});
+
+
+/**************************LOBBY*****************************************/
+
+$("#chatroom1").on("click", function() {
+    $(".landingpagesection").hide();
+    $("#registrationForm").hide();
+    $("#loginForm").hide();
+    $(".lobbyRoom").hide();
+    $(".chatRoom").show();
+});
+
+$(".backLobby").on("click", function() {
+    $(".landingpagesection").hide();
+    $("#registrationForm").hide();
+    $("#loginForm").hide();
+    $(".lobbyRoom").show();
+    $(".chatRoom").hide();
+});
+
 
 /*************************Validating Login Form*************************/
 
-$("#loginBTN").on("click", function () {
+$("#loginBTN").on("click", function (e) {
 let usersUsernameLogin = $("#userNameLogin").val();
     if (usersUsernameLogin === "" || isNaN(usersUsernameLogin) === false) {       //checks if input field is missing or is a number.
         $("#error1Login").text(" *");
@@ -120,7 +181,7 @@ let usersPasswordLogin = $("#passwordLogin").val();
         $("#passwordLogin").css({"borderColor": "#ccc"});
     }
 let usersLoginMessage = $("#loginMessage")
-    if (usersUsernameLogtin === "" || isNaN(usersUsernameLogin) === false ||
+    if (usersUsernameLogin === "" || isNaN(usersUsernameLogin) === false ||
     usersPasswordLogin === "" || usersPasswordLogin.length < 6) {     //checks if the input fields fulfils the specified requirements.
         $("#loginMessage").text("Please fill in the required fields.");
         $("#loginMessage").css({"color": "red"});
@@ -128,6 +189,9 @@ let usersLoginMessage = $("#loginMessage")
         $("#loginMessage").text("Welcome back!");
         $("#loginMessage").css({"color": "green"});
     }
+const auth = firebase.auth();
+const promise = auth.signInWithEmailAndPassword(usersUsernameLogin, usersPasswordLogin);
+
 });
 
 /*************************TEEEEEEEEEEEST*************************/
